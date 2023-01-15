6477bbd20215ec4b5a8bd7d6ebf41a48a79bc9de
xxlee <kochetova.sveta2003@gmail.com>
2023-01-15 21:53:21 +0300

    6 этап

--- Web Quiz Engine/task/src/engine/repositories/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package engine.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends CrudRepository<engine.models.Task, Long> {
-    engine.models.Task findTaskById(Long id);
-}
